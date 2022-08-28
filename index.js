2ce64ae68c2206484d2e873f8b80aa403ce040fa
zzangsally <zzangsally@naver.com>
2022-08-28 23:18:28 +0900

    Filling Table with Orders using JavaScript

--- index.js
+++ index.js
@@ -18,3 +18,20 @@ themeToggler.addEventListener('click', () => {
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 })
+
+// fill orders in table
+Orders.forEach(order => {
+    const tr = document.createElement('tr');
+    const trContent = `
+        <td>${order.productName}</td>
+        <td>${order.productNumber}</td>
+        <td>${order.paymentState}</td>
+        <td class="${order.shipping === '결제 취소' ? 'danger' : order.shipping === '결제중' ? 'warning' : 'primary'}">
+            ${order.shipping}
+        </td>
+        <td class="primary">자세히 보기</td>
+    `;
+
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: '불고기',
+        productNumber: '00326',
+        paymentState: '카드',
+        shipping: '결제 취소',
+    },
+    {
+        productName: '로제 파스타',
+        productNumber: '00826',
+        paymentState: '카드',
+        shipping: '결제중',
+    },
+    {
+        productName: '비빔밥',
+        productNumber: '00506',
+        paymentState: '카드',
+        shipping: '결제 완료',
+    },
+    {
+        productName: '연어 초밥',
+        productNumber: '01127',
+        paymentState: '카드',
+        shipping: '결제 완료',
+    },
+    {
+        productName: '찹스테이크',
+        productNumber: '01112',
+        paymentState: '카드',
+        shipping: '결제중',
+    },
+]
